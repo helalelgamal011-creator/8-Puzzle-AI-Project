79ae21f68a494f824e43b0896eda77e5a22883b9
Helal Elgamal <helalelgamal011@gmail.com>
2025-12-18 20:21:40 +0200

    Refactor UCS function for improved readability

--- UCS/ucs.py
+++ UCS/ucs.py
@@ -1,15 +1,22 @@
 import heapq
-from common.puzzle_utils import get_neighbors, GOAL_STATE
+from common import GOAL_STATE,get_neighbors,print_puzzle
 
 def ucs(start):
-    pq = [(0, start, [])]
-    visited = set()
-
-    while pq:
-        cost, state, path = heapq.heappop(pq)
-        if state == GOAL_STATE:
+    heap=[(0,start,[start])]
+    visited=set()
+    while heap:
+        cost,state,path=heapq.heappop(heap)
+        if state==GOAL_STATE:
             return path
-        if state not in visited:
-            visited.add(state)
-            for n in get_neighbors(state):
-                heapq.heappush(pq, (cost+1, n, path + [n]))
+        if state in visited:
+            continue
+        visited.add(state)
+        for n in get_neighbors(state):
+            if n not in visited:
+                heapq.heappush(heap,(cost+1,n,path+[n]))
+
+if __name__=="__main__":
+    start=(1,2,3,4,0,6,7,5,8)
+    path=ucs(start)
+    for s in path:
+        print_puzzle(s)
