cac45d76676f3cb589eac24c6f3f2f111bb7689f
Helal Elgamal <helalelgamal011@gmail.com>
2025-12-18 20:23:43 +0200

    Update dfs.py

--- DFS/dfs.py
+++ DFS/dfs.py
@@ -1,25 +1,21 @@
-from common import GOAL_STATE,get_neighbors,print_puzzle
+from common import GOAL_STATE, get_neighbors, print_puzzle
 
-def ids(start):
-    depth=0
-    while True:
-        res=dls(start,depth)
-        if res: return res
-        depth+=1
-
-def dls(state,limit,path=None,visited=None):
-    if path is None: path=[state]
-    if visited is None: visited=set()
-    if state==GOAL_STATE: return path
-    if limit<=0: return None
-    visited.add(state)
-    for n in get_neighbors(state):
-        if n not in visited:
-            res=dls(n,limit-1,path+[n],visited.copy())
-            if res: return res
+def dfs(start):
+    stack = [(start,[start])]
+    visited = set()
+    while stack:
+        state, path = stack.pop()
+        if state==GOAL_STATE:
+            return path
+        if state in visited:
+            continue
+        visited.add(state)
+        for n in reversed(get_neighbors(state)):
+            if n not in visited:
+                stack.append((n,path+[n]))
 
 if __name__=="__main__":
-    start=(1,2,3,4,0,6,7,5,8)
-    path=ids(start)
+    start = (1,2,3,4,0,6,7,5,8)
+    path = dfs(start)
     for s in path:
         print_puzzle(s)
